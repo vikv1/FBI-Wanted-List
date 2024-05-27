e83b81296b02a2a1ca404d0a7475660a553d179a
Vikrant Verma <87392159+vikv1@users.noreply.github.com>
2024-05-26 18:43:37 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markk@uw.edu
+ * Date: 7/24/2018
+ * Time: 2:45 PM
+*/?>
+<html>
+<head>
+    <title>THE FBI</title>
+    <link rel="stylesheet" href="base.css">
+</head>
+<body>
+<?php
+require_once 'header.inc.php';
+?>
+<div>
+    <h2>About</h2>
+    <div>the fbi bruh</div>
+</div>
+</body>
+</html>
