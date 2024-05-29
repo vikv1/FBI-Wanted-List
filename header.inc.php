74c9fb6956ea0d720bd6f611c8ae6cf33b135a19
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-05-29 16:27:50 -0700

    changed tab names

--- header.inc.php
+++ header.inc.php
@@ -6,8 +6,8 @@
 <!-- This is a common navigation base to call different web pages on the website -->
 <div class="topnav">
     <a href="index.php">Home</a>
-    <a href="list_criminals.php">List Criminals</a>
-    <a href="list_crimes.php">List Crimes</a>
+    <a href="list_criminals.php">Fake List of Criminals</a>
+    <a href="list_crimes.php">Fake List of Crimes</a>
 </div></div>
 <h1>Fake FBI Most Wanted</h1>
 
