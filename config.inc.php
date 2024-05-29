4c7fa5cf8a08c7ffb6582342b5a8d50ff56c7d28
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-05-28 22:00:24 -0700

    Update config.inc.php
    changed the DB name and added a comment about the port #

--- config.inc.php
+++ config.inc.php
@@ -10,4 +10,9 @@ $servername = 'vergil.u.washington.edu'; // Your Server (if noot)
 $username = 'root';
 $password = 'fbi';
 $database = 'wantedlist';
+// each of our personal ports are different so please make sure that this remains the same
+// Vik's port = 20345
+// Steven's port = 15411
+// Alisha's port =
+// Sukai's port =
 $port = 20345; // Your Port
