aaef9c9536b549d22d8ef9b35bf75a549af53fc7
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-05-29 15:49:06 -0700

    Update header.inc.php

--- header.inc.php
+++ header.inc.php
@@ -8,7 +8,6 @@
     <a href="index.php">Home</a>
     <a href="list_criminals.php">List Criminals</a>
     <a href="list_crimes.php">List Crimes</a>
-    <a href="last_5_queries.php">Last 5 Queries</a>
 </div></div>
 <h1>Fake FBI Most Wanted</h1>
 
